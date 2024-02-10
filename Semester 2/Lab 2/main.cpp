3034b4cfbb5cda8430ee9d6f9931e4386405542b
RomanRudin <58559283+RomanRudin@users.noreply.github.com>
2024-02-10 12:04:33 +0200

    Update main.cpp

--- Semester 2/Lab 2/main.cpp
+++ Semester 2/Lab 2/main.cpp
@@ -3,24 +3,26 @@
 
 int main()
 {
-	float** a = new float*[3];
+	float** a = new float* [3];
 	for (int i = 0; i < 3; i++)
 		a[i] = new float[3];
 
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
 			a[i][j] = (i + 1) * (j + 1);
-	 
-	fs::Matrix C(a, 3, 3);
-	fs::Matrix A(3, 3);
-	fs::Matrix B(2, 2);
+
+	mx::Matrix C(3, 3, a);
+	mx::Matrix A(3, 3);
+	mx::Matrix B(2, 2);
 
 	A.Print();
 	B.Print();
 	C.Print();
 
 	B = A = C;
-	
+
+	std::cout << "After operation: " << std::endl;
+
 	A.Print();
 	B.Print();
 	C.Print();
