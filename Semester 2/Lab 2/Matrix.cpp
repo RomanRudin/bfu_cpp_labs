4af2c99fe3a1f6ab63f916b35eb4a28a9577d136
RomanRudin <58559283+RomanRudin@users.noreply.github.com>
2024-02-10 12:43:15 +0200

    Update Matrix.cpp

--- Semester 2/Lab 2/Matrix.cpp
+++ Semester 2/Lab 2/Matrix.cpp
@@ -3,13 +3,14 @@
 
 namespace mx
 {
-	//Matrix::Matrix() {
-	//	this->n = 1;
-	//	this->m = 1;
-	//	this->data = new float* [1];
-	//	this->data[0] = new float[1];
-	//	this->data[0][0] = 0;
-	//};
+	Matrix::Matrix() {
+		this->n = 1;
+		this->m = 1;
+		this->data = new float* [1];
+		this->data[0] = new float[1];
+		this->data[0][0] = 0;
+		std::cout << "Basic constr\n";
+	};
 
 	Matrix::Matrix(int n, int m) {
 		this->n = n;
@@ -23,10 +24,11 @@ namespace mx
 		for (int i = 0; i < this->n; i++)
 			for (int j = 0; j < this->m; j++)
 				std::cin >> this->data[i][j];
+		std::cout << "Constr\n";
 	};
 
 	Matrix::Matrix(Matrix& other) : Matrix(other.n, other.m, other.data) {
-
+		std::cout << "Copy constr\n";
 	};
 
 	Matrix::Matrix(int n, int m, float**& data) {
@@ -39,12 +41,14 @@ namespace mx
 		for (int i = 0; i < this->n; i++)
 			for (int j = 0; j < this->m; j++)
 				this->data[i][j] = data[i][j];
+		std::cout << "With data constr\n";
 	};
 
 	Matrix::~Matrix() {
 		for (int i = 0; i < this->n; i++)
 			delete[] this->data[i];
 		delete[] this->data;
+		std::cout << "Destr\n";
 	};
 
 	Matrix& Matrix::operator=(Matrix other){
