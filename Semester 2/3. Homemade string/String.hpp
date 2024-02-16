baac46d8e5324635b98ea8a3dc79b342b97d397e
RomanRudin <rudin.roman@bk.ru>
2024-02-17 01:54:09 +0200

    Third lab was finished

--- Semester 2/3. Homemade string/String.hpp
+++ Semester 2/3. Homemade string/String.hpp
@@ -1,14 +1,16 @@
 #pragma once
 #include <iostream>
-namespace str{
+namespace str {
 	class String {
 	private:
 		char* str;
 		size_t len;
 
 	public:
-		String() = default;
+		String();
+		String(size_t length);
 		String(char* data);
+		String(const char* data);
 		~String();
 
 		String(const String& other);
@@ -17,18 +19,21 @@ namespace str{
 		String operator+(const String& other);
 		String& operator+=(const String& other);
 		String operator*(int a);
-		String& operator*=(int a);
+		String& operator*=(int a); //TODO
 
-		char operator[](int index);
+		char& operator[](int index);
 
 		bool operator==(const String& other);
-		bool operator<(const String& str);
-		bool operator>(const String& str);
+		bool operator!=(const String& other);
+		bool operator<(const String& other);
+		bool operator>(const String& other);
+		bool operator<=(const String& other);
+		bool operator>=(const String& other);
 
 		friend std::istream& operator>>(std::istream& in, String& other);
 		friend std::ostream& operator<<(std::ostream& out, String& other);
-	
-		char at(int index);
+
+		char& at(int index);
 		bool isEmpty();
 		int find(char symbol);
 		int find(String& str);
@@ -36,21 +41,21 @@ namespace str{
 		int rfind(String& str);
 		int count(char symbol);
 		int count(String& str);
-		int length();
+		size_t length();
 		char* c_str();
-		
+
 		bool isUpper();
 		bool isLower();
-		void capitalise(); 
+		void capitalise();
 		void toUpper();
-		bool isUpper();
+		bool toLower();
 		bool isDigit();
-		bool isSymbol();
+		bool isLetter();
 
 		void replace(char from, char to);
 		void replace(char from, char to, int repeat);
-		void replace(String& from, String& to);
-		void replace(String& from, String& to, int repeat);
+		void replace(String& from, String& to); //TODO
+		void replace(String& from, String& to, int repeat);//TODO
 	};
 
 	std::istream& operator>>(std::istream& in, String& other);
