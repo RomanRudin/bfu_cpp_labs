402d546c4728aee9ebfe6d0dd0d864a6373c0768
Roman <rudin.roman@bk.ru>
2024-05-22 22:29:00 +0200

    Currently lab 7 is not working...

--- Semester_2/7. FileReader/main.cpp
+++ Semester_2/7. FileReader/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
 // N < 256 и a_i < 256, для всех i=1..N
 // .txt - массив данных в формате N и a_i, где i=1..N (ASCII)
 // .bin - массив данных в формате N и a_i, где i=1..N (bin)
@@ -10,16 +9,14 @@ class DataReader
 {
 protected:
 	std::ifstream m_in;
+	std::ofstream m_out;
 	std::string m_filename;
 	uint8_t* m_data;
 	uint8_t m_n;
 
 public:
 	DataReader(const std::string& filename) :
-		m_filename(filename), m_n(0), m_data(nullptr)
-	{
-
-	}
+		m_filename(filename), m_n(0), m_data(nullptr) {}
 
 	virtual ~DataReader() {}
 
@@ -30,13 +27,24 @@ public:
 	}
 
 	virtual void Read() = 0;
-	virtual void Write() = 0;
+	virtual void Write(std::string filename) = 0;
 
 	void GetData(uint8_t* buf, uint8_t& n)
 	{
 		n = m_n;
 		for (int i = 0; i < m_n; i++)
 			buf[i] = m_data[i];
+	}
+
+	void GetData(float* buf, unsigned int& n)
+	{
+		std::cout << "Getting data" << std::endl;
+		std::cout << "Here:" << "\t" << m_n << "\t" << m_data << std::endl;
+		n = m_n;
+		for (int i = 0; i < m_n; i++) {
+			std::cout << m_data[i] << std::endl;
+			buf[i] = m_data[i];
+		}
 	}
 };
 
@@ -72,9 +80,14 @@ public:
 		}
 	}
 
-	void Write() override
+	void Write(std::string filename) override
 	{
-
+		m_out.open(filename);
+		m_out << m_n << std::endl;
+		for (uint8_t i = 0; i < m_n; i++) {
+			m_out << (int)m_data[i] << ' ';
+		}
+		m_out.close();
 	}
 };
 
@@ -103,36 +116,52 @@ public:
 		m_in.read((char*)m_data, m_n);
 	}
 
-	void Write() override
+	void Write(std::string filename) override
 	{
-
+		m_out.open(filename, std::ios::binary);
+		m_out << m_n;
+		for (uint8_t i = 0; i < m_n; i++) {
+			m_out << m_data[i];
+		}
+		m_out.close();
 	}
 };
 
 class BinfReader : public DataReader {
+private:
+	float* m_data;
+	unsigned int m_n;
 public:
-    BinfReader(const std::string& filename) : DataReader(filename) {}
+	BinfReader(const std::string& filename) : DataReader(filename) {}
 	virtual ~BinfReader()
 	{
 		if (m_data != nullptr)
 			delete[] m_data;
 	}
 
-    bool Open() override {
-        m_in.open(m_filename, std::ios::binary);
-		if (!m_in.is_open()) 
+	bool Open() override {
+		m_in.open(m_filename, std::ios::binary);
+		if (!m_in.is_open())
 			return false;
-        return true;
-    }
+		return true;
+	}
 
-    void Read() override {
+	void Read() override {
 		m_in.read((char*)&m_n, sizeof(int));
 		float* m_data = new float[m_n];
-		for (int i = 0; i < m_n; i++)
-			m_in.read(reinterpret_cast<char*>(&m_data[i]), sizeof(float));
-    }
+		m_in.read((char*)m_data, m_n * sizeof(float));
+		std::cout << "Here I am! " << m_n << "\t" << m_data << std::endl;
+	}
 
-    void Write() override {}
+	void Write(std::string filename) override
+	{
+		m_out.open(filename, std::ios::binary);
+		m_out.write((char*)&m_n, sizeof(uint32_t));
+		for (uint32_t i = 0; i < m_n; i++) {
+			m_out.write((char*)&m_data[i], sizeof(float));
+		}
+		m_out.close();
+	}
 };
 
 DataReader* Factory(const std::string& filename)
@@ -148,20 +177,52 @@ DataReader* Factory(const std::string& filename)
 	return nullptr;
 }
 
+void test_files(std::string name) {
+	uint8_t n;
+	uint8_t buf[100];
+
+	DataReader* Reader = Factory("input" + name);
+	if (Reader == nullptr) {
+		std::cerr << "unable to open: 'input" << name << "'" << std::endl;
+		return;
+	}
+	Reader->Open();
+	Reader->Read();
+	Reader->GetData(buf, n);
+	Reader->Write("output" + name);
+
+	std::cout << std::endl << "input" + name << std::endl;
+	std::cout << (int)n << std::endl;
+	for (int i = 0; i < n; i++)
+		std::cout << (int)buf[i] << std::endl;
+
+	delete Reader;
+}
+
 int main() {
-    uint8_t n;
-    uint8_t buf[100];
+	test_files("1.txt");
+	test_files("2.bin");
 
-    DataReader* Reader = Factory("input3.binf");
-    if (Reader == nullptr)
-        return -1;
-    Reader->Open();
-    Reader->Read();
-    Reader->GetData(buf, n);
+	unsigned int n;
+	float buf[100];
 
-    std::cout << (int)n << std::endl;
-    for (int i = 0; i < n; i++)
-        std::cout << (int)buf[i] << std::endl;
+	DataReader* Reader = Factory("input3.binf");
+	if (Reader == nullptr) {
+		std::cerr << "unable to open: 'input3.binf'" << std::endl;
+		return 0;
+	}
+	Reader->Open();
+	Reader->Read();
+	std::cout << std::endl << "input3.binf" << std::endl;
+	Reader->GetData(buf, n);
+	std::cout << std::endl << "Got data" << std::endl;
+	Reader->Write("output3.binf");
 
-    delete Reader;
+	std::cout << (int)n << std::endl;
+	for (int i = 0; i < n; i++)
+		std::cout << (int)buf[i] << std::endl;
+
+	delete Reader;
+
+	return 1;
 }
